ba4b3503b5d6faf14ede4e59d81e284a0e2b4d2a
Davlatova Madina <muzaffarovna2103@gmail.com>
2025-05-04 19:06:40 +0500

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
+import PlantShop from "../Section2";
 
 const Shop = () => {
   return (
-    <div
-      style={{ height: "400px" }}
-      className="flex items-center justify-center "
-    >
-      <p className="text-red-600 text-xl font-semibold">
-        No selected products
-      </p>
+    <div className="min-h-screen bg-white text-gray-900">
+      <PlantShop />
     </div>
   );
 };
