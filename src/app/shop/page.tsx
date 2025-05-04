4a6844d105ad256871a8f84f3811fa23383f92f5
Davlatova Madina <muzaffarovna2103@gmail.com>
2025-05-04 19:15:31 +0500

    Update page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,7 +3,7 @@ import PlantShop from "../Section2";
 
 const Shop = () => {
   return (
-    <div className="min-h-screen bg-white text-gray-900">
+    <div className="min-h-screen bg-white text-gray-900 mb-5">
       <PlantShop />
     </div>
   );
